c498c704cdc8cf81b447ce22b9271c6d5a8bbcc5
Jimmy Thomson <jithomso@microsoft.com>
2014-11-21 16:12:46 -0800

    First checkin for vs-mda-client command line for communicating with vs-mda-remote.
    If invoked with the correct arguments, this will connect to vs-mda-remote, send a build across (Probably failing to do an incremental build) and if the build is for a device and successful, download the ipa ready to be put on a device.

---
+++ Users/jithomso/vs-mda-client/vs-mda-client/vs-mda-client/vs-mda-client.ts
@@ -0,0 +1,7 @@
+﻿import remoteBuild = require("./lib/remoteBuild");
+import BuildSettings = require("./lib/BuildSettings");
+import Q = require("q");
+
+var settings = new BuildSettings(process.cwd());
+
+Q(remoteBuild.build(settings)).done()
