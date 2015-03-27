15c9ee01aa70aa317ea0ee8374b5a980f74c6dc3
gj <gj>
2015-03-27 11:13:21 -0700

    Initial commit for Create branch

--- src/taco-cli/cli/create.ts
+++ src/taco-cli/cli/create.ts
@@ -2,12 +2,15 @@
 /// <reference path="../../typings/node.d.ts" />
 /// <reference path="../../typings/nopt.d.ts" />
 
-import tacoUtility = require ("taco-utils");
+import tacoUtility = require("taco-utils");
+import utils = tacoUtility.UtilHelper;
 import commands = tacoUtility.Commands;
+import resources = tacoUtility.ResourcesManager;
 import logger = tacoUtility.Logger;
+import cordovaWrapper = require("./utils/cordova-wrapper");
 import level = logger.Level;
-import nopt = require ("nopt");
-import Q = require ("q");
+import nopt = require("nopt");
+import Q = require("q");
 
 /*
  * Create
@@ -16,17 +19,88 @@ import Q = require ("q");
  */
 class Create implements commands.IDocumentedCommand {
     public info: commands.ICommandInfo;
-    /**
-     * Sample only, shows processing specific taco commands, and passing remaining ones to Cordova CLI
-     */  
+
+    private static tacoOnlyOptions: string[] = ["kit", "cli", "template"];
+    private static knownOptions: Nopt.FlagTypeMap = {
+        // taco-cli options
+        "kit": String,
+        "template": String,
+        "cli": String,
+
+        // Cordova options
+        "copy-from": String,
+        "link-to": String
+    };
+
+    private commandData: commands.ICommandData;
+
     public run(data: commands.ICommandData): Q.Promise<any> {
-        logger.logErrorLine("Create not yet implemented");
-        return Q.reject("Not yet implemented");
+        return this.parseArguments(data)
+            .then(this.verifyArguments)
+            .then(this.processKit)
+            .then(this.processTemplate)
+            .then(this.callCordovaCreate)
+            .then(this.copyRemainingTemplateItemsIfNeeded)
+            .then(this.processTemplateTokensIfNeeded);
+    }
+
+    private parseArguments(args: commands.ICommandData): Q.Promise<commands.ICommandData> {
+        this.commandData = utils.parseArguments(Create.knownOptions, {}, args.original, 0);
+        return Q.resolve<any>(null);
+    }
+
+    private verifyArguments(): Q.Promise<any> {
+        // Parameter exclusivity validation and other verifications
+        if (this.commandData.options["template"] && (this.commandData.options["copy-from"] || this.commandData.options["link-to"])) {
+            logger.logErrorLine(resources.getString("command.create.notTemplateIfCustomWww"));
+            return Q.reject("command.create.notTemplateIfCustomWww");
+        }
+
+        if (this.commandData.options["cli"] && this.commandData.options["kit"]) {
+            logger.logErrorLine(resources.getString("command.create.notBothCliAndKit"));
+            return Q.reject("command.create.notBothCliAndKit");
+        }
+
+        if (this.commandData.options["cli"] && this.commandData.options["template"]) {
+            logger.logWarnLine(resources.getString("command.create.notBothTemplateAndCli"));
+        }
+
+        return Q.resolve(null);
+    }
+
+    private processKit(): Q.Promise<any> {
+        return Q.resolve(null);
+    }
+
+    private processTemplate(): Q.Promise<any> {
+        return Q.resolve(null);
+    }
+
+    private callCordovaCreate(): Q.Promise<any> {
+        // Create the flag bag to pass to Cordova, stripping out any flag that is specific to taco-cli
+        var cfg: { [flag: string]: any } = {};
+
+        for (var option in this.commandData.options) {
+            if (Create.tacoOnlyOptions.indexOf(option) === -1) {
+                // This is not an option we understand; pass it through to Cordova
+                cfg[option] = this.commandData.options[option];
+            }
+        }
+
+        return cordovaWrapper.create(this.commandData.remain[0], this.commandData.remain[1], this.commandData.remain[2], this.commandData.remain[3], cfg);
+    }
+
+    private copyRemainingTemplateItemsIfNeeded(): Q.Promise<any> {
+        return Q.resolve(null);
+    }
+
+    private processTemplateTokensIfNeeded(): Q.Promise<any> {
+        return Q.resolve(null);
     }
 
     /**
      * specific handling for whether this command can handle the args given, otherwise falls through to Cordova CLI
-     */  
+     */
     public canHandleArgs(data: commands.ICommandData): boolean {
         return true;
     }
--- src/taco-cli/cli/utils/cordova-wrapper.ts
+++ src/taco-cli/cli/utils/cordova-wrapper.ts
@@ -1,5 +1,5 @@
-﻿import child_process = require ("child_process");
-import Q = require ("q");
+﻿import child_process = require("child_process");
+import Q = require("q");
 
 class CordovaWrapper {
     public static cli(args: string[]): Q.Promise<any> {
@@ -16,6 +16,34 @@ class CordovaWrapper {
 
     public static build(platform: string): Q.Promise<any> {
         return CordovaWrapper.cli(["build", platform]);
+    }
+
+    public static create(path: string, id?: string, name?: string, cdvConfig?: string, cfg?: any): Q.Promise<any> {
+        var command: string[] = ["create", path];
+
+        if (id) {
+            command.push(id);
+        }
+
+        if (name) {
+            command.push(name);
+        }
+
+        if (cdvConfig) {
+            command.push(cdvConfig);
+        }
+
+        // Options
+        for (var option in cfg) {
+            command.push("--" + option);
+
+            // If the property has a value that isn't boolean, also include its value in the command
+            if (cfg[option] && typeof cfg[option] !== "Boolean") {
+                command.push(cfg[option]);
+            }
+        }
+
+        return CordovaWrapper.cli(command);
     }
 }
 
