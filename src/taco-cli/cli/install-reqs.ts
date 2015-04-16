171da3a6aeef2d668602c94643dcc718e53b787a
Guillaume Jenkins <gujenkin@microsoft.com>
2015-04-16 13:38:53 -0700

    shelveset

--- src/taco-cli/cli/install-reqs.ts
+++ src/taco-cli/cli/install-reqs.ts
@@ -1,14 +1,14 @@
 ﻿/// <reference path="../../typings/taco-utils.d.ts" />
 "use strict";
 
-import tacoUtility = require("taco-utils");
+import tacoUtility = require ("taco-utils");
 import utils = tacoUtility.UtilHelper;
 import commands = tacoUtility.Commands;
 import resources = tacoUtility.ResourcesManager;
 import logger = tacoUtility.Logger;
 import level = logger.Level;
-import cordovaWrapper = require("./utils/cordova-wrapper");
-import Q = require("q");
+import cordovaWrapper = require ("./utils/cordova-wrapper");
+import Q = require ("q");
 
 /*
  * InstallReqs
@@ -17,17 +17,14 @@ import Q = require("q");
  */
 class InstallReqs implements commands.IDocumentedCommand {
     private static KnownOptions: Nopt.FlagTypeMap = {
-        kit: String,
-        template: String,
-        cli: String,
-        "copy-from": String,
-        "link-to": String
     };
 
     public info: commands.ICommandInfo;
 
     public run(data: commands.ICommandData): Q.Promise<any> {
         var commandData: commands.ICommandData = utils.parseArguments(InstallReqs.KnownOptions, {}, data.original, 0);
+
+
 
         return Q.resolve({});
     }
