09faef2f106ecc2462fa292f2daf0a691a4bc42d
Nisheet Jain <nisheet.jain@microsoft.com>
2015-04-24 17:06:23 -0700

    Fixing broken help for all commands

--- src/taco-cli/cli/help.ts
+++ src/taco-cli/cli/help.ts
@@ -87,9 +87,13 @@ class Help implements commands.IDocumentedCommand {
         logger.logLine(resourcesManager.getString("command.help.usage.synopsis") + "\n", level.NormalBold);
         logger.logLine(this.indent + this.tacoString + " " + command + " " + list.synopsis + "\n", level.Success);
         logger.logLine(this.getDescriptionString(list.description) + "\n", level.NormalBold);
-        this.printCommandTable(list.args, this.indent);        
+        if (list.args) {
+            this.printCommandTable(list.args, this.indent);
+        }
         logger.logLine("\n" + this.indent + resourcesManager.getString("command.help.usage.options") + "\n", level.NormalBold);
-        this.printCommandTable(list.options, this.indent + this.indent);
+        if (list.options) {
+            this.printCommandTable(list.options, this.indent + this.indent);
+        }
     }
 
     /**
