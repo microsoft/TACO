c7d3bcbd8cea2bfefa1b97e1fd5582c5689e057a
Nisheet Jain <nisheet.jain@microsoft.com>
2015-06-07 16:53:29 -0700

    Adding "Actual" TacoError Codes for taco-kits and taco-utils as discussed

--- src/taco-kits/tacoErrorCodes.ts
+++ src/taco-kits/tacoErrorCodes.ts
@@ -2,13 +2,14 @@
 "use strict";
 
 module TacoKits {
+    // Error Codes: 2000-2099
     export enum TacoErrorCode {
-        TacoKitsExceptionInvalidKit,
-        TacoKitsExceptionInvalidTemplate,
-        TacoKitsExceptionKitMetadataFileMalformed,
-        TacoKitsExceptionKitMetadataFileNotFound,
-        TacoKitsExceptionNoCliSpecification,
-        TacoKitsExceptionTypescriptNotSupported
+        TacoKitsExceptionInvalidKit = 2001,
+        TacoKitsExceptionInvalidTemplate = 2002,
+        TacoKitsExceptionKitMetadataFileMalformed = 2003,
+        TacoKitsExceptionKitMetadataFileNotFound = 2004,
+        TacoKitsExceptionNoCliSpecification = 2005,
+        TacoKitsExceptionTypescriptNotSupported = 2006
     }
 }
 
