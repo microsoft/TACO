ca1dbef84609f67309f1238cdf0f682b6b9e2fd5
Jimmy Thomson <jithomso@microsoft.com>
2015-06-16 15:17:08 -0700

    Adding in a newline normalizing stream NewlineNormalizerStream replaces all newlines (windows or unix style) in its input with OS-dependent newlines in its output.

---
+++ src/typings/newlineNormalizerStream.d.ts
@@ -0,0 +1,23 @@
+﻿/**
+ *******************************************************
+ *                                                     *
+ *   Copyright (C) Microsoft. All rights reserved.     *
+ *                                                     *
+ *******************************************************
+ */
+
+/// <reference path="../typings/node.d.ts" />
+declare module TacoUtility {
+    /**
+     * This class counts how many bytes pass through it in a pipe stream.
+     * Example usage:
+     * var cs = new CountStream();
+     * readableStream.pipe(cs).pipe(writableStream);
+     * [... later]
+     * console.log(cs.count + " bytes written");
+     */
+    class NewlineNormalizerStream extends NodeJSStream.Transform {
+        constructor(options?: NodeJSStream.TransformOptions);
+        _transform(chunk: any, encoding: string, callback: (err: Error, buf: string) => void): void;
+    }
+}
