f2fead50dd764bbb92641c5bb85cf9e57a95bba1
Jimmy Thomson <jithomso@microsoft.com>
2015-07-17 17:54:00 -0700

    Adding missing typings for nconf

---
+++ src/typings/nconf_extensions.d.ts
@@ -0,0 +1,7 @@
+﻿declare module "nconf" {
+    export function save(value?: any, callback?: Function): any;
+    export function merge(value: {}): any;
+    export function merge(value: any, key: string): any;
+    export function merge(callback: Function, value: {}): any;
+    export function merge(callback: Function, value: any, key: string): any;
+}
