1642a1c750a9eb3d7251c7656cfcedbc3fa2d262
Jimmy Thomson <jithomso@microsoft.com>
2015-08-21 21:18:49 -0700

    Adding missing file (the logo!) and postinstall logo script

---
+++ src/taco-cli/cli/logo.ts
@@ -0,0 +1,26 @@
+/**
+ *******************************************************
+ *                                                     *
+ *   Copyright (C) Microsoft. All rights reserved.     *
+ *                                                     *
+ *******************************************************
+ */
+// NOTE: This file is intended to run on-require, and print the logo, only once.
+var version = require("../package.json").version;
+
+console.log("_____________________________");
+console.log("___  __/__    |_  ____/_  __ \\");
+console.log("__  /  __  /| |  /    _  / / /");
+console.log("_  /   _  ___ / /___  / /_/ /");
+var lstLn = "/_/    /_/  |_\\____/  \\____/   CLI v" + version;
+console.log(lstLn);
+console.log();
+
+var len = lstLn.length;
+var line = new Array(len + 1).join("-");
+var title = "Tools for Apache Cordova";
+var spaces = Math.floor((len - title.length) / 2);
+
+console.log(line);
+console.log(new Array(spaces + 1).join(" ") + title);
+console.log(line);
