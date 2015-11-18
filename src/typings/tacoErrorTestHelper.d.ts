a585ee5d1f12832bd222498a696583cafb1d17e4
Nisheet Jain <nisheet.jain@microsoft.com>
2015-11-17 22:43:46 -0800

    Moving tacoErrorTestHelper to taco-test-utils and fixing all the references to use taco-test-utils.TacoErrorTestHelper instead

---
+++ src/typings/tacoErrorTestHelper.d.ts
@@ -0,0 +1,16 @@
+/**
+ *******************************************************
+ *                                                     *
+ *   Copyright (C) Microsoft. All rights reserved.     *
+ *                                                     *
+ *******************************************************
+ */
+
+/// <reference path="../typings/node.d.ts" />
+
+declare module TacoTestsUtils {
+    class TacoErrorTestHelper {
+        public static verifyTacoErrors(fileName: string, resources: any, minErrorCode: number, maxErrorCode: number): void;
+    }
+}
+
