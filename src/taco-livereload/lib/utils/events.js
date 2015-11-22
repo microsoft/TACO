f0688017746b43ed3cb86dae6fdb40bc6ab2363e
Omar Mefire <omefire@gmail.com>
2015-11-21 23:43:52 -0800

    Added a new package that offers the livereload functionality: taco-livereload

---
+++ src/taco-livereload/lib/utils/events.js
@@ -0,0 +1,10 @@
+/**
+﻿ *******************************************************
+﻿ *                                                     *
+﻿ *   Copyright (C) Microsoft. All rights reserved.     *
+﻿ *                                                     *
+﻿ *******************************************************
+﻿ */
+
+var events = require('events');
+module.exports = new events.EventEmitter();
