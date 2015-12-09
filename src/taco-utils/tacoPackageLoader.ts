a3cca0fe1534c18324bf1c2ee70bc7848570bdaa
leolee <leolee@microsoft.com>
2015-12-08 18:10:24 -0800

    Override cordova version
    testing with Cordova@5.4.1 in teamcity
    
    Enable ability to override version of Cordova used in LazyRequire to gate check new versions of Cordova
    
    "gulp run-tests --testCordova 5.4.1"
    
    Details:
    1. put "TestCordovaExempt" in scenario name to exempt from gate check
    2. exempt text is inserted into args to mocha.opts
    i.e. --invert --grep TestCordovaExempt
    3. version of Cordova used is stored in process.env.testCordovaVersion
    
    Refactored code into lazyCordovaRequire and lazyCordovaRun

--- src/taco-utils/tacoPackageLoader.ts
+++ src/taco-utils/tacoPackageLoader.ts
@@ -88,15 +88,17 @@ module TacoUtility {
     };
 
     export interface ITacoPackageLoader {
+        lazyCordovaRequire<T>(cordovaCliVersion: string, logLevel?: InstallLogLevel): Q.Promise<T>;
         lazyRequire<T>(packageName: string, packageId: string, logLevel?: InstallLogLevel): Q.Promise<T>;
-        lazyRun(packageName: string, packageId: string, commandName: string, logLevel?: InstallLogLevel): Q.Promise<string>;
+        lazyCordovaRun(cordovaCliVersion: string, logLevel?: InstallLogLevel): Q.Promise<string>;
+        lazyRun(packageName: string, packageId: string, commandName: string, logLevel?: InstallLogLevel): Q.Promise<string>;        
     }
 
     export class TacoPackageLoader {
         /* tslint:disable:member-ordering */
         // Need to declare FILE_REGEX_PREFIX before FILE_URI_REGEX, but don't want to expose it outside the class
         private static FILE_REGEX_PREFIX: string = "file://";
-
+        private static CORDOVA_NPM_PACKAGE_NAME: string = "cordova";
         public static GIT_URI_REGEX: RegExp = /^http(s?)\\:\/\/.*|.*\.git$/;
         public static FILE_URI_REGEX: RegExp = new RegExp("^" + TacoPackageLoader.FILE_REGEX_PREFIX + ".*");
 
@@ -117,27 +119,24 @@ module TacoUtility {
          */
         public static lazyRun(packageName: string, packageId: string, commandName: string, logLevel: InstallLogLevel = InstallLogLevel.warn): Q.Promise<string> {
             var request: IPackageInstallRequest = TacoPackageLoader.createPackageInstallRequest(packageName, packageId, logLevel);
+            return TacoPackageLoader.installAndGetExecutable(packageName, commandName, request); 
+        }
 
-            return Q({})
-                .then(function (): Q.Promise<void> {
-                    return TacoPackageLoader.installPackageIfNeeded(request);
-                })
-                .then(function (): Q.Promise<string> {
-                    var packageJsonFilePath = path.join(request.targetPath, "package.json");
-                    var packageJson = JSON.parse(fs.readFileSync(packageJsonFilePath, "utf8"));
-
-                    if (packageJson.bin && packageJson.bin[commandName]) {
-                        var commandFilePath = path.join(request.targetPath, "..", ".bin", commandName);
-                        if (os.platform() === "win32") {
-                            commandFilePath += ".cmd";
-                        }
-                        if (fs.existsSync(commandFilePath)) {
-                            return Q.resolve(commandFilePath);
-                        }
-                    }
-
-                    return Q.reject<string>(errorHelper.get(TacoErrorCodes.PackageLoaderRunPackageDoesntHaveRequestedBinary, packageName, commandName));
-                });
+        /**
+         * Returns a path to the specified command exported from the specified Cordova package. If the package is not already downloaded,
+         * then first download and cache it locally.
+         *
+         * @param {string} packageName The name of the package to load
+         * @param {string} packageId The version of the package to load. Either a version number such that "npm install package@version" works, or a git url to clone
+         * @param {string} commandName The name of the binary to find
+         * @param {LogLevel} logLevel Optional parameter which determines how much output from npm is filtered out. 
+         *                  Follows the npm syntax: silent, warn, info, verbose, silly
+         *                  loglevel can also be used as "pretty" in which case, only formatted taco messages like Downloading cordova@5.0 are shown
+         * @returns {Q.Promise<string>} A promise which is either rejected with a failure to find the local the binary or resolved with a path to the binary
+         */
+        public static lazyCordovaRun(cordovaCliVersion: string, logLevel: InstallLogLevel = InstallLogLevel.warn): Q.Promise<string> {
+            var request: IPackageInstallRequest = TacoPackageLoader.createCordovaPackageInstallRequest(cordovaCliVersion, logLevel);
+            return TacoPackageLoader.installAndGetExecutable(TacoPackageLoader.CORDOVA_NPM_PACKAGE_NAME, "cordova", request);
         }
 
         /**
@@ -161,6 +160,34 @@ module TacoUtility {
                 return TacoPackageLoader.lazyRequireInternal<T>(TacoPackageLoader.createPackageInstallRequest(packageName, packageId, logLevel));
             } else {
                 return this.mockForTests.lazyRequire<T>(packageName, packageId, logLevel);
+            }
+        }
+
+        /**
+         * Load a Cordova package with specified version. If the package is not already downloaded,
+         * then first download the package and cache it locally for future loads. The loaded package is cast to type T
+         *
+         * This method is resilient against interrupted downloads, but is not safe under concurrency.
+         * Until that changes, we should not allow multiple builds at once.
+         *
+         * @param {string} packageVersion The version of the package to load. Either a version number such that "npm install package@version" works, or a git url to clone
+         * @param {string} logLevel Optional parameter which determines how much output from npm is filtered out. 
+         *                  Follows the npm syntax: silent, warn, info, verbose, silly
+         *                  loglevel can also be used as "taco" in which case, only formatted taco messages like Downloading cordova@5.0 are shown
+         * 
+         *
+         * @returns {Q.Promise<T>} A promise which is either rejected with a failure to install, or resolved with the require()'d package
+         */
+        public static lazyCordovaRequire<T>(cordovaCliVersion: string, logLevel: InstallLogLevel = InstallLogLevel.warn): Q.Promise<T> {
+            if (!this.mockForTests) {
+                var request: IPackageInstallRequest = TacoPackageLoader.createCordovaPackageInstallRequest(cordovaCliVersion, logLevel);
+
+                return TacoPackageLoader.lazyRequireInternal<T>(request);
+            } else {
+                return this.mockForTests.lazyRequire<T>(
+                    TacoPackageLoader.CORDOVA_NPM_PACKAGE_NAME,
+                    TacoPackageLoader.CORDOVA_NPM_PACKAGE_NAME + "@" + cordovaCliVersion,
+                    logLevel);
             }
         }
 
@@ -320,6 +347,42 @@ module TacoUtility {
                 case PackageSpecType.Error:
                     throw errorHelper.get(TacoErrorCodes.PackageLoaderInvalidPackageVersionSpecifier, packageId.split("@")[1], packageName);
             }
+        }
+
+        private static createCordovaPackageInstallRequest(cordovaCliVersion: string, logLevel: InstallLogLevel): IPackageInstallRequest {
+            var request: IPackageInstallRequest = TacoPackageLoader.createPackageInstallRequest(
+                TacoPackageLoader.CORDOVA_NPM_PACKAGE_NAME,
+                TacoPackageLoader.CORDOVA_NPM_PACKAGE_NAME + "@" + cordovaCliVersion,
+                logLevel);
+
+            if (process.env.testCordovaVersion) {
+                request.packageId = "cordova@" + process.env.testCordovaVersion;
+            }
+
+            return request;
+        }
+
+        private static installAndGetExecutable(packageName: string, commandName: string, request: IPackageInstallRequest): Q.Promise<string> {
+            return Q({})
+                .then(function (): Q.Promise<void> {
+                    return TacoPackageLoader.installPackageIfNeeded(request);
+                })
+                .then(function (): Q.Promise<string> {
+                    var packageJsonFilePath = path.join(request.targetPath, "package.json");
+                    var packageJson = JSON.parse(fs.readFileSync(packageJsonFilePath, "utf8"));
+
+                    if (packageJson.bin && packageJson.bin[commandName]) {
+                        var commandFilePath = path.join(request.targetPath, "..", ".bin", commandName);
+                        if (os.platform() === "win32") {
+                            commandFilePath += ".cmd";
+                        }
+                        if (fs.existsSync(commandFilePath)) {
+                            return Q.resolve(commandFilePath);
+                        }
+                    }
+
+                    return Q.reject<string>(errorHelper.get(TacoErrorCodes.PackageLoaderRunPackageDoesntHaveRequestedBinary, packageName, commandName));
+                });
         }
 
         private static createTacoPackageInstallRequest(packageKey: string, dependencyConfigPath: string, logLevel: InstallLogLevel): IPackageInstallRequest {
--- src/typings/tacoPackageLoader.d.ts
+++ src/typings/tacoPackageLoader.d.ts
@@ -42,6 +42,37 @@ declare module TacoUtility {
         public static lazyRun(packageName: string, packageId: string, commandName: string, logLevel?: InstallLogLevel): Q.Promise<string>;
 
         /**
+         * Load a Cordova package with specified version. If the package is not already downloaded,
+         * then first download the package and cache it locally for future loads. The loaded package is cast to type T
+         *
+         * This method is resilient against interrupted downloads, but is not safe under concurrency.
+         * Until that changes, we should not allow multiple builds at once.
+         *
+         * @param {string} packageVersion The version of the package to load. Either a version number such that "npm install package@version" works, or a git url to clone
+         * @param {string} logLevel Optional parameter which determines how much output from npm is filtered out. 
+         *                  Follows the npm syntax: silent, warn, info, verbose, silly
+         *                  loglevel can also be used as "taco" in which case, only formatted taco messages like Downloading cordova@5.0 are shown
+         * 
+         *
+         * @returns {Q.Promise<T>} A promise which is either rejected with a failure to install, or resolved with the require()'d package
+         */
+        public static lazyCordovaRequire<T>(cordovaCliVersion: string, logLevel?: InstallLogLevel): Q.Promise<T>;
+
+        /**
+         * Returns a path to the specified command exported from the specified Cordova package. If the package is not already downloaded,
+         * then first download and cache it locally.
+         *
+         * @param {string} packageName The name of the package to load
+         * @param {string} packageId The version of the package to load. Either a version number such that "npm install package@version" works, or a git url to clone
+         * @param {string} commandName The name of the binary to find
+         * @param {LogLevel} logLevel Optional parameter which determines how much output from npm is filtered out. 
+         *                  Follows the npm syntax: silent, warn, info, verbose, silly
+         *                  loglevel can also be used as "pretty" in which case, only formatted taco messages like Downloading cordova@5.0 are shown
+         * @returns {Q.Promise<string>} A promise which is either rejected with a failure to find the local the binary or resolved with a path to the binary
+         */
+        public static lazyCordovaRun(cordovaCliVersion: string, logLevel?: InstallLogLevel): Q.Promise<string>;
+
+        /**
          * Load a node package with specified version. If the package is not already downloaded,
          * then first download the package and cache it locally for future loads. The loaded package is cast to type T
          *
