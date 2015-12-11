07147fb26712ed8deb0b9c3583c00f0d8a3e0c3c
Joshua Skelton <joskelto@microsoft.com>
2015-12-10 16:39:02 -0800

    Updated NpmHelper from PR feedback.

--- src/typings/npmHelper.d.ts
+++ src/typings/npmHelper.d.ts
@@ -8,10 +8,11 @@
 
 /// <reference path="../typings/installLogLevel.d.ts" />
 /// <reference path="../typings/q.d.ts" />
+
 declare module TacoUtility {
     class NpmHelper {
-        public static install(args: string[], path: string, logLevel?: InstallLogLevel): Q.Promise<any>;
-		
-		public static view(args: string[], path: string, logLevel?: InstallLogLevel): Q.Promise<any>;
+        public static install(packageId: string, workingDirectory?: string, commandFlags?: string[], logLevel?: InstallLogLevel): Q.Promise<any>;
+
+		public static view(packageId: string, fields?: string[], workingDirectory?: string, commandFlags?: string[], logLevel?: InstallLogLevel): Q.Promise<any>;
     }
 }
