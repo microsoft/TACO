bfc18ccb61f76d19871f15abb5be94868c735c70
Guillaume Jenkins <gujenkin@microsoft.com>
2015-12-15 09:29:23 -0800

    Removed references to VisualStudio

--- templates/default/blank/www/scripts/platformOverrides.js
+++ templates/default/blank/www/scripts/platformOverrides.js
@@ -1,4 +1,1 @@
-/*
-	This file is replaced with platform-specific code from the /merges folder.
-	More info at http://taco.visualstudio.com/en-us/docs/configure-app/#Content.
-*/
+// Platform specific overrides will be placed in the merges folder versions of this file 		
