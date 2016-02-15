2c512c462df8a6ffe83832b704eee038eb3b5779
Andre Asselin <andre.asselin@pointsource.com>
2016-02-15 17:38:17 -0500

    Switch to using ATS remover from cordova-browsersync-primitives

--- src/taco-livereload/lib/utils/Patcher.js
+++ src/taco-livereload/lib/utils/Patcher.js
@@ -12,7 +12,7 @@ var multiPlatforms = require('./platforms');
 var url = require('url');
 var path = require('path');
 var promiseUtils = require('./promise-util');
-var ATSRemover = require('./ATSRemover');
+var browserSyncPrimitives = require('cordova-browsersync-primitives');
 var CSPRemover = require('./CSPRemover');
 var multiPlatforms = require('./platforms');
 
@@ -31,9 +31,9 @@ function Patcher(projectRoot, platforms) {
  *    - Removes ATS (App Transport Security) in the case of iOS9+
  *    - Copies the homePage to the mobile app
  *    - Changes the start page of the app to the homePage
- * 
+ *
  * HomePage Role:
- *    - The homePage allows us to ping the BrowserSync server 
+ *    - The homePage allows us to ping the BrowserSync server
  *    - If we can't connect to the BrowserSync server, we display a meaningful error message to the user
  *    - If we can successfully connect to the BrowserSync server, we proceed with loading the app
  */
@@ -44,13 +44,13 @@ Patcher.prototype.patch = function (serverUrl) {
         var platformIndexLocal = path.join(self.projectRoot, platWWWFolder, self.startPage);
         var cspRemover = new CSPRemover(platformIndexLocal);
         return cspRemover.Remove().then(function () {
-            var atsRemover = new ATSRemover(self.projectRoot, plat);
-            return atsRemover.Remove();
+            if (plat == 'ios')
+                browserSyncPrimitives.fixATS(self.projectRoot, helpers.GetProjectName(self.projectRoot));
         }).then(function () {
             var platformIndexUrl = url.resolve(serverUrl, path.join(multiPlatforms.getPlatformWWWFolder(plat), self.startPage));
             return copyHomePage(self.projectRoot, plat, platformIndexUrl);
         }).then(function (homePage) {
-            return helpers.ChangeStartPage(self.projectRoot, plat, homePage); 
+            return helpers.ChangeStartPage(self.projectRoot, plat, homePage);
         });
     });
 };
@@ -72,22 +72,22 @@ Patcher.prototype.removeCSP = function () {
 
 // Copy the homePage.html that comes with livereload into the platform's folder
 function copyHomePage(projectRoot, platform, platformIndexUrl) {
-    
-    var HOME_PAGE = 'homePage.html'; 
-    
+
+    var HOME_PAGE = 'homePage.html';
+
     var src = path.join(__dirname, HOME_PAGE);
-    
+
     var dest = path.join(projectRoot, multiPlatforms.getPlatformWWWFolder(platform), HOME_PAGE);
-    
-    // Append random string to the homePage.html filename being copied into the platforms folder 
+
+    // Append random string to the homePage.html filename being copied into the platforms folder
     // ... so that it doesn't clash with potential user-defined homePage.html files
     var randomizedString = '_' + Math.random() + '.html';
     dest += randomizedString;
-    
+
     // Copy from src to dest
     var srcContent = fs.readFileSync(src, 'utf-8');
-    fs.writeFileSync(dest, srcContent.replace(/__SERVER_URL__/g, platformIndexUrl)); 
-    
+    fs.writeFileSync(dest, srcContent.replace(/__SERVER_URL__/g, platformIndexUrl));
+
     return HOME_PAGE + randomizedString;
 }
 
